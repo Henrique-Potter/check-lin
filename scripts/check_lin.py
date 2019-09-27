6cd704dd1085b451323be96738a5361a5a194971
hpotter <hep37@pitt.edu>
2019-09-27 17:52:22 -0400

    Adding support argument switch -o --operation

--- scripts/check_lin.py
+++ scripts/check_lin.py
@@ -3,6 +3,8 @@ from tools.compileTests import compile_tests
 from tools.createTests import create_tests
 from tools.processStats import process_dir, stats_headers
 from tools.processLinStats import process_lin_stats, lin_stats_headers
+from pathlib import Path
+
 
 import argparse
 import time
@@ -10,16 +12,16 @@ import os
 import sys
 
 
+curr_dir = Path(os.path.realpath(__file__)).parents[1]
+
 min_depth = 1
 max_depth = 5
 
 # prepare result files
-result_history = "results/table1.txt"
-result_schedule = "results/table2.txt"
-result_linear = "results/table3.txt"
+result_history = curr_dir.joinpath("results/table1.txt")
+result_schedule = curr_dir.joinpath("results/table2.txt")
+result_linear = curr_dir.joinpath("results/table3.txt")
 
-
-# Other tests commented for a faster execution
 BENCHMARKS =[
               ("ArrayBlockingQueue", "ABQ", "java.util.concurrent.ArrayBlockingQueue<Integer>")
               # ("ConcurrentHashMap", "CHM", "java.util.concurrent.ConcurrentHashMap<Integer,Integer>"),
@@ -115,7 +117,7 @@ def sbt_create_tests():
 
         # History and Schedule Results
         print("Writing history and schedule results: [%s] -> [%s, %s]" % (benchmark, result_history, result_schedule))
-        process_dir(name, os.path.join("stat", stat_subdir), result_history, result_schedule)
+        process_dir(name, os.path.join(curr_dir, "stat", stat_subdir), result_history, result_schedule)
 
     elapsed_secs = time.time() - start
     print("Test creation successful: %s" % elapsed_secs)
@@ -129,7 +131,7 @@ def sbt_compile_tests():
         for d in range(min_depth, max_depth + 1):
             print("Compiling linearizability checking tests: [%s] [d=%s]" % (benchmark, d))
             test_path = "produced/test{0}/D{1}".format(name, d)
-            compile_tests(test_path)
+            compile_tests(curr_dir.joinpath(test_path))
 
     elapsed_secs = time.time() - start
     print("Java files compiled successfully: %s" % elapsed_secs)
@@ -141,13 +143,26 @@ def java_execute_tests():
     for (benchmark, name, classUnderTest) in BENCHMARKS:
         # Compile and test the generated .java files
         for d in range(min_depth, max_depth + 1):
-            test_path = "produced/test{0}/D{1}".format(name, d)
-            out_file = "out/{0}/D{1}.txt".format(name, d)
+
+            test_path = curr_dir
+            java_class_path = "produced/test{0}/D{1}".format(name, d)
+            out_file = curr_dir.joinpath("out/{0}/D{1}.txt".format(name, d))
             print("Running Linearizability checking tests:[%s] [d=%s]" % (benchmark, d))
-            run_tests(test_path, out_file)
+
+            run_tests(java_class_path, test_path, out_file)
+
+            # Count linearizable histories:
+            process_lin_stats(name, out_file, d, result_linear)
+
     elapsed_secs = time.time() - start
     print("Java Linearizability tests successfully completed: %s" % elapsed_secs)
     print("Please call the appropriated command to plot results: %s" % elapsed_secs)
+
+
+def set_project_root():
+    for path in Path.cwd().parents:
+        if path.name == 'check_lin':
+            return path
 
 
 def plot_results():
@@ -161,7 +176,7 @@ if __name__ == '__main__':
         short_name = sys.argv[1]
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("operation", type=str, required=True)
+    parser.add_argument("-o", "--operation", help="Please, specify one operation!", type=str, required=True)
 
     args = parser.parse_args()
 
@@ -171,7 +186,5 @@ if __name__ == '__main__':
         sbt_compile_tests()
     elif args.operation == "java_execute_tests":
         java_execute_tests()
-    elif args.operation == "plot_tests":
+    elif args.operation == "plot_results":
         plot_results()
-
-    #main(short_name)
